5066bff374315777770e4755d9629267bedb9ba8
Octi Zhang <zhengyuz@nvidia.com>
2025-06-27 14:26:46 -0700

    introduce NO_CHANGE token to prevent modify_env_param from doing anything, and wrote test for this modify_env_param and modify_term_cfg

--- source/isaaclab/isaaclab/envs/mdp/curriculums.py
+++ source/isaaclab/isaaclab/envs/mdp/curriculums.py
@@ -45,8 +45,36 @@ class modify_env_param(ManagerTermBase):
     This term compiles getter/setter accessors for a target attribute (specified by
     `cfg.params["address"]`) the first time it is called, then on each invocation
     reads the current value, applies a user-provided `modify_fn`, and writes back
-    the result.
+    the result. Since None in this case can sometime be desirable value to write, we
+    use token, NO_CHANGE, as non-modification signal to this class, see usage below.
+    
+    Usage:
+        .. code-block:: python
+            def resample_bucket_range(
+                env, env_id, data, static_friction_range, dynamic_friction_range, restitution_range, num_steps
+            ):
+                if env.common_step_counter > num_steps:
+                    range_list = [static_friction_range, dynamic_friction_range, restitution_range]
+                    ranges = torch.tensor(range_list, device="cpu")
+                    new_buckets = math_utils.sample_uniform(ranges[:, 0], ranges[:, 1], (len(data), 3), device="cpu")
+                    return new_buckets
+                return mdp.modify_env_param.NO_CHANGE
+
+            object_physics_material_curriculum = CurrTerm(
+                func=mdp.modify_env_param,
+                params={
+                    "address": "event_manager.cfg.object_physics_material.func.material_buckets",
+                    "modify_fn": resample_bucket_range,
+                    "modify_params": {
+                        "static_friction_range": [.5, 1.],
+                        "dynamic_friction_range": [.3, 1.],
+                        "restitution_range": [0.0, 0.5],
+                        "num_step": 120000
+                    }
+                }
+            )
     """
+    NO_CHANGE = object()
 
     def __init__(self, cfg, env):
         """
@@ -82,8 +110,7 @@ class modify_env_param(ManagerTermBase):
             env: The learning environment.
             env_ids: Sub-environment indices (unused by default).
             address: dotted path of the value retrieved from env.
-            modify_fn: Function signature
-                `fn(env, env_ids, old_value, **modify_params) -> new_value`.
+            modify_fn: Function signature `fn(env, env_ids, old_value, **modify_params) -> new_value`.
             modify_params: Extra keyword arguments for `modify_fn`.
         """
         if not self.get_fn:
@@ -91,7 +118,8 @@ class modify_env_param(ManagerTermBase):
 
         data = self.get_fn()
         new_val = modify_fn(self._env, env_ids, data, **modify_params)
-        self.set_fn(new_val)
+        if new_val is not self.NO_CHANGE: # if the modify_fn return NO_CHANGE signal, do not invoke self.set_fn
+            self.set_fn(new_val)
 
     def _compile_accessors(self, root, path: str):
         """
@@ -161,8 +189,23 @@ class modify_term_cfg(modify_env_param):
 
     Reads `cfg.params["address"]`, replaces only the first occurrence of "s."
     with "_manager.cfg.", and then behaves identically to ModifyEnvParam.
-
     for example: command_manager.cfg.object_pose.ranges.xpos -> commands.object_pose.ranges.xpos
+    
+    Usage:
+        .. code-block:: python
+            def override_value(env, env_ids, data, value, num_steps):
+                if env.common_step_counter > num_steps:
+                    return value
+                return mdp.modify_term_cfg.NO_CHANGE
+            
+            command_object_pose_xrange_adr = CurrTerm(
+                func=mdp.modify_term_cfg,
+                params={
+                    "address": "commands.object_pose.ranges.pos_x",   # note that `_manager.cfg` is omitted
+                    "modify_fn": override_value,
+                    "modify_params": {"value": (-.75, -.25), "num_steps": 12000}
+                }
+            )
     """
 
     def __init__(self, cfg, env):
